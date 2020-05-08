7b5ca2a2f34f9c57e72a8abe9e6579a1a23a2a9f
Isol Sigurdardottir <isols19@ru.is>
2020-05-08 00:10:50 +0000

    Vörur komnar inn

--- accessory/views.py
+++ accessory/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from accessory.models import Accessory
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'accessory/index.html')
+    accessories = {'accessories': Accessory.objects.all().order_by('name')}
+    return render(request, 'accessory/index.html', context=accessories)
