40709cf3ff72a3f80da7146f0f6bcefa2b61d66a
Isol Sigurdardottir <isols19@ru.is>
2020-05-08 10:26:03 +0000

    aðeins búið að laga headerinn

--- homepage/urls.py
+++ homepage/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('', views.index, name="homepage"),
     path('openinghours/', views.opening_hours, name='opening-hours-index'),
     path('aboutus/', views.about_us, name='about-us-index')
 ]
