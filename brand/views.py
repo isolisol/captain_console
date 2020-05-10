7cec0fae17ab4e5f628fd6d49aa38dbcaaa2d161
Sunna Run Thorarinsdottir <sunnat16@ru.is>
2020-05-10 18:34:45 +0000

    push

--- brand/views.py
+++ brand/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-#from videogame.models import Brand
+from videogame.models import Brand
 
 # Create your views here.
 def index(request):
-    #context = {'brands': Brand.objects.all().order_by('name')}
-    return render(request, 'brand/index.html')
+    brands = {'brands': Brand.objects.all().order_by('name')}
+    return render(request, 'brand/index.html', context=brands)
