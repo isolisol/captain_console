e1ceb5a6de8378fc4467525e78bbc6f53bf19bcc
Isol Sigurdardottir <isols19@ru.is>
2020-05-10 17:11:48 +0000

    games by brand

--- videogame/urls.py
+++ videogame/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="videogame-index"),
-    path('<int:id>', views.get_videogame_by_id, name="videogame_detail")
+    path('<int:id>', views.get_videogame_by_id, name="videogame_detail"),
+    path('playstation_games', views.get_videogames_by_playstation, name="playstation_games")
 ]
--- videogame/views.py
+++ videogame/views.py
@@ -12,3 +12,9 @@ def get_videogame_by_id(request, id):
     return render(request, 'videogame/videogame_detail.html', context={
         'videogame': get_object_or_404(Product, pk=id)
     })
+
+def get_videogames_by_playstation(request):
+    playstation1 = Product.objects.filter(type_id=2, console_id=6)
+    playstation2 = Product.objects.filter(type_id=2, console_id=7)
+    videogames = {'videogames': playstation1.union(playstation2)}
+    return render(request, 'videogame/videogame_brand.html', videogames)
