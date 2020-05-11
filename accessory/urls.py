ff08e86554c7856e7316ab5ad8ef6f18ed21eb75
hafdis18 <56306203+hafdis18@users.noreply.github.com>
2020-05-11 15:41:04 +0000

    accessories

--- accessory/urls.py
+++ accessory/urls.py
@@ -7,5 +7,12 @@ urlpatterns = [
     path('playstation_accessories', views.get_playstation_accessories, name="playstation_accessories"),
     path('nintendo_accessories', views.get_nintendo_accessories, name="nintendo_accessories"),
     path('xbox_accessories', views.get_xbox_accessories, name="xbox_accessories"),
-    path('gameboy_accessories', views.get_gameboy_accessories, name="gameboy_accessories")
+    path('gameboy_accessories', views.get_gameboy_accessories, name="gameboy_accessories"),
+    path('other_accessories', views.get_other_accessories, name="other_accessories"),
+    path('ps1_accessories', views.get_ps1_accessories, name="ps1_accessories"),
+    path('ps2_accessories', views.get_ps2_accessories, name="ps2_accessories"),
+    path('nintendo_nes_accessories', views.get_nintendo_nes_accessories, name="nintendo_nes_accessories"),
+    path('nintendo_64_accessories', views.get_nintendo_64_accessories, name="nintendo_64_accessories"),
+    path('gameboy_color_accessories', views.get_gameboy_color_accessories, name="gameboy_color_accessories"),
+    path('gameboy_advance_accessories', views.get_gameboy_advance_accessories, name="gameboy_advance_accessories")
 ]
