37fd8e1674e895d38202164da20b6e235a37630e
Isol Sigurdardottir <isols19@ru.is>
2020-05-12 20:56:22 +0000

    remove from cart komið

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from order import views as o_views
 
 urlpatterns = [
     path('', views.index, name="homepage"),
     path('openinghours/', views.opening_hours, name='openinghours'),
     path('aboutus/', views.about_us, name='aboutus'),
+    path('remove_from_cart/<int:product_id>', o_views.remove_from_cart, name="p_remove_from_cart"),
     #path('search_result/' , views.search_products, name='search_result')
 ]
