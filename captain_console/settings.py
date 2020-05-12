5e999d8ed27713a82eaf9f5af2f0092f3b06f9b1
Isol Sigurdardottir <isols19@ru.is>
2020-05-12 13:29:20 +0000

    postal code farið úr settings.py

--- captain_console/settings.py
+++ captain_console/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_countries',
-    'postalcodes',
     'brand.apps.BrandConfig',
     'videogame.apps.VideogameConfig',
     'console.apps.ConsoleConfig',
