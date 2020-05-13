8cdddd76e9174b8e35f34a403fef51d269378036
Isol Sigurdardottir <isols19@ru.is>
2020-05-13 10:01:41 +0000

    remove from cart í dropdown komið alls staðar

--- homepage/urls.py
+++ homepage/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('openinghours/', views.opening_hours, name='openinghours'),
     path('aboutus/', views.about_us, name='aboutus'),
     path('remove_from_cart/<int:product_id>', o_views.remove_from_cart, name="p_remove_from_cart"),
+    path('openinghours/remove_from_cart/<int:product_id>', o_views.remove_from_cart, name="op_remove_from_cart"),
+    path('aboutus/remove_from_cart/<int:product_id>', o_views.remove_from_cart, name="ab_remove_from_cart"),
     #path('search_result/' , views.search_products, name='search_result')
 ]
